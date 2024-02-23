3d31d479ce8d8a0e11c5cb166fafcbd8a1c4a727
Głocki Kacper <glockikacper2@gmail.com>
2024-02-23 16:10:59 +0100

    23,02,2024

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryBox = galleryItems =>
+  galleryItems
+    .map(
+      galleryItem =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"</img>
+  </a>
+  </li>`
+    )
+    .join('');
+const createGalleryBox = galleryBox(galleryItems);
+gallery.innerHTML = createGalleryBox;
 
-console.log(galleryItems);
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'goit-example-message';
+const input = document.querySelector('name');
+const textArea = document.querySelector('email');
 
+form.elements.email.value = localStorage.getItem(localStorageKeyOne) ?? '';
+form.elements.message.value = localStorage.getItem(localStorageKeyTwo) ?? '';
+form.addEventListener('input', evt => {
+  localStorage.setItem(localStorageKeyOne, evt.target.value);
+  localStorage.setItem(localStorageKeyTwo, evt.target.value);
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKeyOne);
+  localStorage.removeItem(localStorageKeyTwo);
+  form.reset();
+});
+
+try {
+  const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (parsedData.email !== '' && parsedData.message !== '') {
+    emailInput.value = parsedData.email;
+    messageInput.value = parsedData.message;
+  } else {
+  }
+} catch (error) {
+  console.log('parsedData error: ' + error);
+}
